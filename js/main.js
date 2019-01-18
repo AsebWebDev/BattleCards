0b7749f3eada69c940964e871c353a2c54ac00e2
André Sebastian <a.sebastian@posteo.de>
2019-01-18 23:31:16 +0100

    add 6 random Cards to DOM on game start

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 /*jshint esversion: 6 */
 
+// G E T   D O M 
+let cardStack = $("#card-stack");
+let allCards = $(".bc");
+
 // G A M E    I N I T
 
-let game = new Game(cardsArray);
+let game = new Game(cardsArray); //array comes from Character.js
 let player1 = new Player();
 let player2 = new Player();
 
-shuffle(game.cardsArray);
-printCharacters();
+printCharacters(); // to console, just for Dev
+fillCardStack();
+
+
 
 // F U N C T I O N S
 
@@ -16,12 +22,25 @@ function shuffle(array) {
   while (0 !== currentIndex) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
   return array;
+}
+
+function fillCardStack(){
+  let htmlBc = "";
+  htmlBc += '<div class="bc">';
+  htmlBc += '<img>';
+  htmlBc += '<p></p>';
+
+  shuffle(game.cardsArray);
+  for (let i = 0; i < 6; i++) {
+    cardStack.append(htmlBc);
+    $("#card-stack .bc:last img").attr("src", game.cardsArray[i].img)
+
+  }
 }
 
 function findWinner (BC1property, BC2property){
