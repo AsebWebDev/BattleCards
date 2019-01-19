35db0246808eb63b90de116cc0986872dcfb0f86
aropexxx <arope@posteo.de>
2019-01-19 09:23:47 +0100

    use vh/vw for responsive battecards

--- js/main.js
+++ js/main.js
@@ -29,21 +29,23 @@ function shuffle(array) {
   return array;
 }
 
-function fillCardStack(){
-  let htmlBc = "";
-  htmlBc += '<div class="bc">';
-  htmlBc += '<img>';
-  htmlBc += '<p></p>';
-
+function fillCardStack() {
   shuffle(game.cardsArray);
-  for (let i = 0; i < 6; i++) {
-    cardStack.append(htmlBc);
-    $("#card-stack .bc:last img").attr("src", game.cardsArray[i].img)
 
+  for (let i = 0; i < 6; i++) {
+    let htmlBc = "";
+    htmlBc += '<div class="bc">';
+    htmlBc += '<img src=' + game.cardsArray[i].img + '>';
+    htmlBc += '<span>' + game.cardsArray[i].name + '</span>';
+    htmlBc += '<div class="properties">'
+    htmlBc += '<p>Strength: ' + game.cardsArray[i].strength + '</p>'
+    cardStack.append(htmlBc);
+    // $("#card-stack .bc:last img").attr("src", game.cardsArray[i].img)
+    // $("#card-stack .bc:last p:first-of-type").text(game.cardsArray[i].name);
   }
 }
 
-function findWinner (BC1property, BC2property){
+function findWinner(BC1property, BC2property) {
   if (BC1property === BC2property) return 0;    // returns 0, if equal
   else if (BC1property > BC2property) return 1; // returns 1, if Winner is Player1
   else if (BC1property < BC2property) return 2; // returns 2, if WInner is Player 2
