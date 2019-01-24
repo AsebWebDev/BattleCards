5054998ed7feb21803ec25038f9a0953a4413e4e
André Sebastian <a.sebastian@posteo.de>
2019-01-24 10:25:37 +0100

    add a bunch of new characters

--- js/Character.js
+++ js/Character.js
@@ -15,15 +15,195 @@ class Character {
 
 // Character Images
 let imgNonBinary = "img/images.jpeg";
-let imgPinkKirby = "img/DJY6Lz2.png";
+let imgPinkKirby = "img/DJY6Lz2.jpg";
 let imgLinkHero = "img/link.png";
 let imgDumbZombie = "img/zombie.jpg";
 let imgCuteMonster = "img/cuteMonster.jpg";
 let imgStrongWarrior = "img/strongWarrior.jpg";
 let imgBlooper = "img/Blooper_MK7.png";
 let imgCrazyCat = "img/cat.gif";
+let imgCuriousAlien = "img/alien.gif";
+let imgLovingBlobber = "img/blobber.gif";
+let imgAngryBomb = "img/bomb.gif";
+let imgYummyCupcake = "img/cupcake.gif";
+let imgMissEgg = "img/egg.gif";
+let imgHairyHarry = "img/hairy.gif";
+let imgJumpyJenny = "img/jumpy.gif";
+let imgMoss = "img/moss.gif";
+let imgBuuHuu = "img/buuhuu.jpg";
+let imgWebDev = "img/nerdy.gif";
+let imgRowdyRocker = "img/rocker.gif";
+let imgLateLinda = "img/running.gif";
+let imgFox = "img/spritefox.gif";
+let imgHero = "img/spritehero.gif";
+let imgWorkoutWilli = "img/toru.gif";
+let imgTrump = "img/trump.gif";
+let imgWhistle = "img/whistleMonster.gif";
+let imgShadowZombie = "img/zombieshadow.gif";
+
 
 // Characters
+let ShadowZombie = new Character(
+  "Shadow Zombie", // Name
+  imgShadowZombie, // Image
+  8, // Strength
+  1, // Intelligence
+  1, // Humor
+  4 // Cuteness
+);
+
+let Whistle = new Character(
+  "Whistle", // Name
+  imgWhistle, // Image
+  3, // Strength
+  3, // Intelligence
+  5, // Humor
+  8 // Cuteness
+);
+
+let Trump = new Character(
+  "Trump", // Name
+  imgTrump, // Image
+  7, // Strength
+  1, // Intelligence
+  1, // Humor
+  1 // Cuteness
+);
+
+let WorkoutWilli = new Character(
+  "Workout Willi", // Name
+  imgWorkoutWilli, // Image
+  5, // Strength
+  7, // Intelligence
+  3, // Humor
+  7 // Cuteness
+);
+
+let Hero = new Character(
+  "Sprite Hero", // Name
+  imgHero, // Image
+  9, // Strength
+  7, // Intelligence
+  5, // Humor
+  3 // Cuteness
+);
+
+let Fox = new Character(
+  "BitFox", // Name
+  imgFox, // Image
+  4, // Strength
+  10, // Intelligence
+  2, // Humor
+  8 // Cuteness
+);
+
+let LateLinda = new Character(
+  "Late Linda", // Name
+  imgLateLinda, // Image
+  5, // Strength
+  8, // Intelligence
+  6, // Humor
+  5 // Cuteness
+);
+
+let RowdyRocker = new Character(
+  "Rowdy", // Name
+  imgRowdyRocker, // Image
+  8, // Strength
+  4, // Intelligence
+  3, // Humor
+  4 // Cuteness
+);
+
+let WebDev = new Character(
+  "WebDev", // Name
+  imgWebDev, // Image
+  2, // Strength
+  10, // Intelligence
+  7, // Humor
+  4 // Cuteness
+);
+
+let BuuHuu = new Character(
+  "Buu Huu", // Name
+  imgBuuHuu, // Image
+  3, // Strength
+  2, // Intelligence
+  2, // Humor
+  5 // Cuteness
+);
+
+let Moss = new Character(
+  "Moss", // Name
+  imgMoss, // Image
+  2, // Strength
+  10, // Intelligence
+  10, // Humor
+  10 // Cuteness
+);
+
+let JumpyJenny = new Character(
+  "Jumpy Jenny", // Name
+  imgJumpyJenny, // Image
+  5, // Strength
+  7, // Intelligence
+  8, // Humor
+  6 // Cuteness
+);
+
+let HairyHarry = new Character(
+  "Hairy Harry", // Name
+  imgHairyHarry, // Image
+  5, // Strength
+  2, // Intelligence
+  2, // Humor
+  6 // Cuteness
+);
+
+let MissEgg = new Character(
+  "Miss Egg", // Name
+  imgMissEgg, // Image
+  5, // Strength
+  7, // Intelligence
+  6, // Humor
+  4 // Cuteness
+);
+
+let YummyCupcake = new Character(
+  "Yummy Cupcake", // Name
+  imgYummyCupcake, // Image
+  10, // Strength
+  4, // Intelligence
+  1, // Humor
+  5 // Cuteness
+);
+
+let AngryBomb = new Character(
+  "Angry Bomb", // Name
+  imgAngryBomb, // Image
+  10, // Strength
+  4, // Intelligence
+  1, // Humor
+  5 // Cuteness
+);
+
+let LovingBlobber = new Character(
+  "Loving Blobber", // Name
+  imgLovingBlobber, // Image
+  2, // Strength
+  4, // Intelligence
+  3, // Humor
+  8 // Cuteness
+);
+
+let CuriousAlien = new Character(
+  "Curious Alien", // Name
+  imgCuriousAlien, // Image
+  1, // Strength
+  8, // Intelligence
+  1, // Humor
+  5 // Cuteness
+);
 
 let crazyCat = new Character(
   "Crazy Cat", // Name
@@ -105,5 +285,23 @@ let cardsDatabase = [
   nonBinary,
   pinkKirby,
   blooper,
-  crazyCat
+  crazyCat,
+  CuriousAlien,
+  LovingBlobber,
+  AngryBomb,
+  YummyCupcake,
+  MissEgg,
+  HairyHarry,
+  JumpyJenny,
+  Moss,
+  BuuHuu,
+  WebDev,
+  RowdyRocker,
+  LateLinda,
+  Fox,
+  Hero,
+  WorkoutWilli,
+  Trump,
+  Whistle,
+  ShadowZombie
 ];
